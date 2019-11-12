b7d22b85a40195110a5d8ec3962c0a8d81de450b
SPS <simpson.gsd@gmail.com>
2019-11-12 07:46:30 +0000

    separate trace offline added no slow safety checks to cargo

--- src/trace.rs
+++ src/trace.rs
@@ -127,6 +127,69 @@ impl TraceSceneBatchJob {
 
     fn trace(&mut self) {
 
+        //self.num_frames += if self.num_frames == 500 {0} else {1};
+        self.num_frames += 1;//if self.num_frames == 500 {0} else {1};
+        let read_state = self.shared_scene_read_state.read();
+
+        if read_state.config.realtime && random::rand() < CHANCE_TO_SKIP_TASK_PER_FRAME {
+            self.shared_scene_write_state.notify_task_completion();
+            return;
+        }
+
+        let local_enable_render: bool = ENABLE_RENDER && !self.realtime;
+
+        for (row_idx, j) in (self.start_xy.1..self.end_xy.1).rev().enumerate() {
+
+            let stride = (self.num_pixels_xy.0 * 4) as usize;
+
+            let start = (self.start_xy.0 * 4 + j * self.image_size.0 * 4) as usize;
+            let dest_buffer_row_slice = &mut self.shared_scene_write_state.buffer.write()[start..start + stride];
+
+            for (col_idx, i) in (self.start_xy.0..self.end_xy.0).enumerate() {
+
+               if read_state.config.realtime && random::rand() < CHANCE_TO_SKIP_PIXEL_PER_FRAME {
+                   continue;
+               }
+
+                let local_pixel_idx = row_idx * self.num_pixels_xy.0 as usize + col_idx;
+                self.num_frames_per_pixel[local_pixel_idx] += if self.num_frames_per_pixel[local_pixel_idx] <= 1000 {1} else {0};
+
+                let mut pixel_colour = Vec3::new_zero_vector();
+                for _ in 0..self.num_samples {
+                    let random = random::rand();
+                    let u: f64 = ((i as f64) + random) / (self.image_size.0 as f64);
+                    let random = random::rand();
+                    let v: f64 = ((j as f64) + random) / (self.image_size.1 as f64);
+
+                    let r = read_state.cam.get_ray(u, v);
+                    pixel_colour += color(&r, &read_state.world, 0, read_state.time0, read_state.time1, read_state.sky_brightness, read_state.disable_emissive);
+
+                    // SS: Debug uv image
+                    // col += Vec3::new(u, v, 0.0);
+                }
+
+                // PDF
+                pixel_colour = pixel_colour / self.num_samples as f64;
+
+                let index = col_idx*4 as usize;
+
+                let num_frames = self.num_frames_per_pixel[local_pixel_idx];
+                let weight = 1.0 / num_frames as f32;
+                let one_minus_weight: f32 = 1.0 - weight;
+
+                dest_buffer_row_slice[index]     = (pixel_colour.z as f32) * weight + dest_buffer_row_slice[index    ] * one_minus_weight;
+                dest_buffer_row_slice[index + 1] = (pixel_colour.y as f32) * weight + dest_buffer_row_slice[index + 1] * one_minus_weight;
+                dest_buffer_row_slice[index + 2] = (pixel_colour.x as f32) * weight + dest_buffer_row_slice[index + 2] * one_minus_weight;
+
+            }
+        }
+
+        // notify completion by decrementing task counter
+        self.shared_scene_write_state.notify_task_completion();
+    }
+
+    fn trace_offline(&mut self) {
+
         let update_window_and_release_lock = |buffer: &mut Vec<u8>, window: &winit::window::Window, image_start_xy: (u32,u32), num_pixels_xy: (u32,u32), window_lock: &AtomicBool| {
             // TODO(SS): Optimise so we are only copying the changed buffer parts
             update_window_framebuffer_rect(&window, buffer, image_start_xy, num_pixels_xy);
@@ -183,38 +246,27 @@ impl TraceSceneBatchJob {
                 pixel_colour = pixel_colour / self.num_samples as f64;
 
                 let index = col_idx*4 as usize;
-                if read_state.config.realtime {
+                // only required if rendering during trace
+                if local_enable_render {
 
-                    let num_frames = self.num_frames_per_pixel[local_pixel_idx];
-                    let weight = 1.0 / num_frames as f32;
-                    let one_minus_weight: f32 = 1.0 - weight;
+                    let tonemapped_col = reinhard_tonemap(&pixel_colour);
 
-                    dest_buffer_row_slice[index]     = (pixel_colour.z as f32) * weight + dest_buffer_row_slice[index    ] * one_minus_weight;
-                    dest_buffer_row_slice[index + 1] = (pixel_colour.y as f32) * weight + dest_buffer_row_slice[index + 1] * one_minus_weight;
-                    dest_buffer_row_slice[index + 2] = (pixel_colour.x as f32) * weight + dest_buffer_row_slice[index + 2] * one_minus_weight;
-                } else {
-
-                    // only required if rendering during trace
-                    if local_enable_render {
-                        let tonemapped_col = reinhard_tonemap(&pixel_colour);
-
-                        // Gamma correct 1/2.0 and convert to u8
-                        let ir = (255.99*tonemapped_col.x.sqrt()) as u8;
-                        let ig = (255.99*tonemapped_col.y.sqrt()) as u8;
-                        let ib = (255.99*tonemapped_col.z.sqrt()) as u8;
-                        
-                        self.local_buffer_u8[buffer_offset]  = ib;
-                        buffer_offset += 1;
-                        self.local_buffer_u8[buffer_offset]  = ig;
-                        buffer_offset += 1;
-                        self.local_buffer_u8[buffer_offset]  = ir;
-                        buffer_offset += 1;
-                    }
-
-                    dest_buffer_row_slice[index] = pixel_colour.b() as f32;
-                    dest_buffer_row_slice[index + 1] = pixel_colour.g() as f32;
-                    dest_buffer_row_slice[index + 2] = pixel_colour.r() as f32;
+                    // Gamma correct 1/2.0 and convert to u8
+                    let ir = (255.99*tonemapped_col.x.sqrt()) as u8;
+                    let ig = (255.99*tonemapped_col.y.sqrt()) as u8;
+                    let ib = (255.99*tonemapped_col.z.sqrt()) as u8;
+                    
+                    self.local_buffer_u8[buffer_offset]  = ib;
+                    buffer_offset += 1;
+                    self.local_buffer_u8[buffer_offset]  = ig;
+                    buffer_offset += 1;
+                    self.local_buffer_u8[buffer_offset]  = ir;
+                    buffer_offset += 1;
                 }
+
+                dest_buffer_row_slice[index] = pixel_colour.b() as f32;
+                dest_buffer_row_slice[index + 1] = pixel_colour.g() as f32;
+                dest_buffer_row_slice[index + 2] = pixel_colour.r() as f32;
             }
 
             if local_enable_render && j % RENDER_UPDATE_LATENCY == 0 && self.shared_scene_write_state.window_lock.compare_and_swap(false, true, Ordering::Acquire) {
@@ -238,7 +290,11 @@ impl TraceSceneBatchJob {
 
 impl JobTask for TraceSceneBatchJob {
     fn run(&mut self) {
-        self.trace();
+        if self.realtime {
+            self.trace();
+        } else {
+            self.trace_offline();
+        }
     }
 }
 
