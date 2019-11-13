4da7f422a2ec6ff86eda3bee09d6a9b268b5c030
SPS <simpson.gsd@gmail.com>
2019-11-13 15:57:45 +0000

    more perlin changes WIP

--- src/math/noise.rs
+++ src/math/noise.rs
@@ -1,5 +1,10 @@
 use math::vec3::Vec3;
 use crate::random;
+use crate::vec3;
+
+fn hermite_cubic(x: f64) -> f64 {
+    x * x * (3.0 - 2.0 * x)
+}
 
 fn trillinear_interpolate(c: &[[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
     let mut accum = 0.0;
@@ -8,7 +13,6 @@ fn trillinear_interpolate(c: &[[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64
         for j in 0..2 {
             for k in 0..2 {
                 let (i_f64, j_f64, k_f64) = (i as f64, j as f64, k as f64);
-               // let weight_v = Vec3::new(u - i_f64, v - j_f64, w - k_f64);
                 accum += (i_f64 * u + (1.0 - i_f64) * (1.0 - u)) *
                          (j_f64 * v + (1.0 - j_f64) * (1.0 - v)) *
                          (k_f64 * w + (1.0 - k_f64) * (1.0 - w)) * c[i][j][k];
@@ -19,11 +23,34 @@ fn trillinear_interpolate(c: &[[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64
     accum
 }
 
+fn perlin_interpolate(c: &[[[Vec3; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+    let uu = hermite_cubic(u);
+    let vv = hermite_cubic(v);
+    let ww = hermite_cubic(w);
+    let mut accum = 0.0;
+    for i in 0..2 {
+        for j in 0..2 {
+            for k in 0..2 {
+                let (i_f64, j_f64, k_f64) = (i as f64, j as f64, k as f64);
+                let weight_v = Vec3::new(u - i_f64, v - j_f64, w - k_f64);
+                accum += (i_f64 * uu + (1.0 - i_f64) * (1.0 - uu)) *
+                         (j_f64 * vv + (1.0 - j_f64) * (1.0 - vv)) *
+                         (k_f64 * ww + (1.0 - k_f64) * (1.0 - ww)) * vec3::dot(&c[i][j][k], &weight_v);
+            }
+        }
+    }
 
-fn perlin_generate() -> [f64;256] {
-    let mut p = [0.0; 256];
+    accum
+}
+
+
+fn perlin_generate() -> [Vec3;256] {
+    let mut p = [Vec3::from_float(0.0); 256];
     for elem in p.iter_mut() {
-        *elem = random::rand();
+        let x_random = 2.0 * random::rand() - 1.0;
+        let y_random = 2.0 * random::rand() - 1.0;
+        let z_random = 2.0 * random::rand() - 1.0;
+        *elem = Vec3::new_unit_vector(&Vec3::new(x_random, y_random, z_random));
     }
 
     p
@@ -49,11 +76,12 @@ fn perlin_generate_perm() -> [i32; 256] {
 }
 
 lazy_static::lazy_static!{
-    static ref RAN_FLOAT: [f64; 256] = perlin_generate();
+    static ref RAN_VEC: [Vec3; 256] = perlin_generate();
     static ref PERM_X: [i32; 256] = perlin_generate_perm();
     static ref PERM_Y: [i32; 256] = perlin_generate_perm();
     static ref PERM_Z: [i32; 256] = perlin_generate_perm();
 }
+
 
 
 pub struct Perlin;
@@ -66,22 +94,14 @@ impl Perlin {
         let v = p.y - j as f64;
         let w = p.z - k as f64;
 
-        let hermite_cubic = |x: f64| -> f64 {
-            x*x*(3.0 - 2.0*x)
-        };
-
-        let u = hermite_cubic(u);
-        let v = hermite_cubic(v);
-        let w = hermite_cubic(w);
-
-        let mut c = [[[0.0; 2]; 2]; 2];
+        let mut c = [[[Vec3::from_float(0.0); 2]; 2]; 2];
         for di in 0..2 {
             for dj in 0..2 {
                 for dk in 0..2 {
                     let di_i32 = di as i32;
                     let dj_i32 = dj as i32;
                     let dk_i32 = dk as i32;
-                    c[di][dj][dk] = RAN_FLOAT[
+                    c[di][dj][dk] = RAN_VEC[
                         (PERM_X[(i+di_i32 & 255) as usize] ^ 
                          PERM_Y[(j+dj_i32 & 255) as usize] ^ 
                          PERM_Z[(k+dk_i32 & 255) as usize]) as usize
@@ -89,6 +109,6 @@ impl Perlin {
                 }
             }
         }
-        trillinear_interpolate(&c, u, v, w)
+        perlin_interpolate(&c, u, v, w)
     }
 }
